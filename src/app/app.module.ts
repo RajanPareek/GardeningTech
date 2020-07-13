ddd7b8b10bb0e8b8dbc7caa396a16974fb7a5571
Rajan Pareek <rpareek@ecash.com.au>
2020-07-13 16:47:52 +1000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './sections/topbar/topbar.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutusComponent } from './sections/aboutus/aboutus.component';
+import { ContactusComponent } from './sections/contactus/contactus.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { FooterComponent } from './sections/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
